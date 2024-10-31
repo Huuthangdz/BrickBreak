539dd351c7f69987086ba8078306adc64389c5b5
Nguyễn Hữu Thắng <149491159+Huuthangdz@users.noreply.github.com>
2024-10-31 11:34:33 +0700

    assets

---
+++ Assets/GameController2.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameController2 : Singleton<GameController>
+{
+    [SerializeField] GameObject Player;
+
+    public GameObject TextLevel;
+    public TextMeshProUGUI TextCount;
+
+    private int timer = 0;
+
+    private void Start()
+    {
+        StartAction();
+    }
+    public void StartAction()
+    {
+        timer = 5;
+        TextCount.text = timer.ToString();
+        TextLevel.SetActive(true);
+        StartCoroutine(CoroutineDelayAction());
+    }
+
+    IEnumerator CoroutineDelayAction()
+    {
+        for (int i = timer; i > -1; i--)
+        {
+            yield return new WaitForSeconds(1f);
+            TextCount.text = i.ToString();
+        }
+        TextLevel.SetActive(false);
+    }
+}
